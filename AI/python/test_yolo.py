4ae64e67fe7c7198afa7ff19bae24705cc1fdf23
RubenGadd2 <u23633353@tuks.co.za>
2025-06-23 17:53:34 +0200

    Updated a couple of linting things + fix's

--- AI/python/test_yolo.py
+++ AI/python/test_yolo.py
@@ -1,5 +1,14 @@
 from ultralytics import YOLO
 
-model = YOLO("yolov5s.pt")  # Loads pre-trained YOLOv5s model
-results = model.predict("https://ultralytics.com/images/bus.jpg")
-print(results[0].boxes.xyxy)  # Print detected bounding boxes
+try:
+    model = YOLO("yolov5su.pt")
+except FileNotFoundError:
+    print("Local model not found, downloading...")
+    model = YOLO("yolov5su.pt")
+try:
+    results = model.predict("bus.jpg")
+except FileNotFoundError:
+    print("Local image not found, downloading...")
+    results = model.predict("https://ultralytics.com/images/bus.jpg")
+
+print(results[0].boxes.xyxy)
