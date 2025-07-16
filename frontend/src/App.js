7ff19911cad68c614854e565261e5e9510a27515
EsterhuizenRuan <ruan.esterhuizen123@gmail.com>
2025-07-16 17:35:50 +0200

    header and navbar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,17 +6,20 @@ import MapPage from './pages/MapPage';
 import CapturePage from './pages/CapturePage';
 import FeedScreen from './pages/FeedPage';
 import ProfilePage from './pages/ProfilePage';
+import Layout from './components/Layout';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
+        <Route element={<Layout /> }>
+          <Route path="/main" element={<MainScreen />} />
+          <Route path="/map" element={<MapPage />} />
+          <Route path="/capture" element={<CapturePage />} />
+          <Route path="/feed" element={<FeedScreen />} />
+          <Route path="/profile" element={<ProfilePage />} />
+        </Route>
         <Route path="/" element={<AuthScreen />} />
-        <Route path="/main" element={<MainScreen />} />
-        <Route path="/map" element={<MapPage />} />
-        <Route path="/capture" element={<CapturePage />} />
-        <Route path="/feed" element={<FeedScreen />} />
-        <Route path="/profile" element={<ProfilePage />} />
       </Routes>
     </BrowserRouter>
   );
