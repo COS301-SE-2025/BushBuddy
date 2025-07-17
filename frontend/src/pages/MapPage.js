14392559a908bd9ab1d1f71c9bc0d70bd0ef6788
EsterhuizenRuan <ruan.esterhuizen123@gmail.com>
2025-07-17 18:41:04 +0200

    map page css fix

--- frontend/src/pages/MapPage.js
+++ frontend/src/pages/MapPage.js
@@ -1,28 +1,38 @@
 import React from 'react';
-import { useNavigate } from 'react-router-dom';
 import { Container } from 'react-bootstrap';
-import './MapPage.css';
-import { MapContainer, TileLayer } from 'react-leaflet';
+import { MapContainer, TileLayer, LayersControl } from 'react-leaflet';
 import 'leaflet/dist/leaflet.css';
+import './MapPage.css';
 
 const MapPage = () => {
-    const position = [-25.7461, 28.1881];
+  const position = [-25.8812222, 28.291611111111113]; //starting location, should be init to user location
 
-    return (
-        <Container className="map-page">
-            <MapContainer
-                center={[-25.7461, 28.1881]}
-                zoom={13}
-                scrollWheelZoom={true}
-                style={{ height: '100%', width: '100%' }}
-            >
-                <TileLayer
-                    attribution='&copy; OpenStreetMap contributors'
-                    url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-                />
-            </MapContainer>
-        </Container>
-    );
+  return (
+    <Container className="map-page">
+      <MapContainer
+        center={position}
+        zoom={13}
+        scrollWheelZoom={true}
+        style={{ height: '100%', width: '100%' }}
+        className='map-container'
+      >
+        <LayersControl position="topright">
+          <LayersControl.BaseLayer checked name="OpenStreetMap">
+            <TileLayer
+              attribution='&copy; OpenStreetMap contributors'
+              url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+            />
+          </LayersControl.BaseLayer>
+          <LayersControl.BaseLayer name="Esri Satellite">
+            <TileLayer
+              attribution='Tiles &copy; Esri &mdash; Source: Esri, i-cubed, USDA, USGS, AEX, GeoEye, etc.'
+              url="https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}"
+            />
+          </LayersControl.BaseLayer>
+        </LayersControl>
+      </MapContainer>
+    </Container>
+  );
 };
 
 export default MapPage;
