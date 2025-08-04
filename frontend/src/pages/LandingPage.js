5acc8cb6cf5dce0f295a225eb183dfe36c5c8a53
EsterhuizenRuan <ruan.esterhuizen123@gmail.com>
2025-08-04 10:31:20 +0200

    small css fix

--- frontend/src/pages/LandingPage.js
+++ frontend/src/pages/LandingPage.js
@@ -3,22 +3,24 @@ import { Container, Button } from 'react-bootstrap';
 import { useNavigate } from 'react-router-dom'
 import './LandingPage.css'
 import InstallPrompt from '../components/InstallPrompt';
+import logo from "../assets/BushBuddy.webp"
 
 const LandingPage = () => {
   const navigate = useNavigate();
 
-  
+  //ToDo: When user management is implemented, swap this page with a splash screen
 
   return (
     <Container className="landing-page">
       <Container className="heading-container">
+        <img src={logo} alt="BushBuddy Logo" className="logo-image"></img>
         <h1>Welcome to BushBuddy!</h1>
         <h2>Track, Discover, Share</h2>
       </Container>
 
       <Container className="actions-container">
-        <Button onClick={() => navigate("/login")} className="button">Log in/Register</Button>
         <InstallPrompt />
+        <Button onClick={() => navigate("/login")} className="button">Log in/Register</Button>
       </Container>
 
       
