b2ee68064081c4f45c36ff30a9de2080fdd617e7
RaphaelRato <raphaelrato2002@gmail.com>
2025-08-16 23:55:06 +0200

    Made feed card changes for new post returns

--- frontend/src/components/FeedCard.jsx
+++ frontend/src/components/FeedCard.jsx
@@ -1,25 +1,27 @@
 import React from 'react';
 import './FeedCard.css';
+import { FaLocationArrow } from "react-icons/fa";
 
 const FeedCard = ({ entry }) => {
   return (
     <div className="feed-card">
       <div className="feed-card-header">
-        <img src={entry.userAvatar} alt={`${entry.user}'s avatar`} className="avatar" />
-        <span className="username">{entry.user}</span>
+        {/*insert user avatar for src={entry.userAvatar}*/}
+        <img src={require('../assets/Jean-Steyn-ProfilePic.webp')} alt={`${entry.user}'s avatar`} className="avatar" />
+        <span className="username">{entry.user_id}</span>
       </div>
-
-      <img src={entry.image} alt={entry.title} className="feed-card-image" />
+      {/*insert alt={entry.title} if needed*/}
+      <img src={entry.image_url} alt={entry.title} className="feed-card-image" />
 
       <div className="feed-card-body">
-        <p className="description"><strong>{entry.user}</strong> {entry.description}</p>
+        <p className="description"><strong>{entry.user_id}</strong> {entry.description}</p>
         <div className="engagement">
           <span className="likes">❤️ {entry.likes}</span>
           <span className="comments">💬 {entry.comments}</span>
         </div>
         <div className="feed-card-footer">
-        <span className="timestamp">{entry.timestamp}</span>
-        <span className="location">{entry.location}</span>
+        <span className="timestamp">{entry.created_at}</span>
+        {/*<span className="location"><FaLocationArrow />{entry.location}</span>*/}
       </div>
       </div>
     </div>
