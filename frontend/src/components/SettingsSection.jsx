de16c4b0c2070abfc8d568f0a19cd4955d0c3788
RaphaelRato <raphaelrato2002@gmail.com>
2025-08-18 23:07:39 +0200

    Added popup to all unimplemented buttons

--- frontend/src/components/SettingsSection.jsx
+++ frontend/src/components/SettingsSection.jsx
@@ -1,37 +1,47 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './SettingsSection.css';
+import PopUpModal from './PopUpModal';
+
 import { FaEnvelope, FaLock, FaQuestionCircle, FaInfoCircle, FaSignOutAlt } from 'react-icons/fa';
 
-const SettingsSection = ({ onLogout }) => (
-  <div className="settings-section">
-    <div className="settings-section-title">Account</div>
-    <div className="settings-list">
-      <div className="settings-row" tabIndex={0} role="button">
-        <FaEnvelope className="settings-icon" />
-        <span>Change Email</span>
-        <span className="settings-arrow">{'>'}</span>
+const SettingsSection = ({ onLogout }) => {
+  const [showPopup, setShowPopup] = useState(false);
+
+  return (
+    <div className="settings-section">
+      <div className="settings-section-title">Account</div>
+      <div className="settings-list">
+        <div className="settings-row" tabIndex={0} role="button" onClick={() => setShowPopup(true)}>
+          <FaEnvelope className="settings-icon" />
+          <span>Change Email</span>
+          <span className="settings-arrow">{'>'}</span>
+        </div>
+        <div className="settings-row" tabIndex={0} role="button" onClick={() => setShowPopup(true)}>
+          <FaLock className="settings-icon" />
+          <span>Change Password</span>
+          <span className="settings-arrow">{'>'}</span>
+        </div>
+        <div className="settings-row" tabIndex={0} role="button" onClick={() => setShowPopup(true)}>
+          <FaQuestionCircle className="settings-icon" />
+          <span>Help & Support</span>
+          <span className="settings-arrow">{'>'}</span>
+        </div>
+        <div className="settings-row" tabIndex={0} role="button" onClick={() => setShowPopup(true)}>
+          <FaInfoCircle className="settings-icon" />
+          <span>About App</span>
+          <span className="settings-arrow">{'>'}</span>
+        </div>
       </div>
-      <div className="settings-row" tabIndex={0} role="button">
-        <FaLock className="settings-icon" />
-        <span>Change Password</span>
-        <span className="settings-arrow">{'>'}</span>
-      </div>
-      <div className="settings-row" tabIndex={0} role="button">
-        <FaQuestionCircle className="settings-icon" />
-        <span>Help & Support</span>
-        <span className="settings-arrow">{'>'}</span>
-      </div>
-      <div className="settings-row" tabIndex={0} role="button">
-        <FaInfoCircle className="settings-icon" />
-        <span>About App</span>
-        <span className="settings-arrow">{'>'}</span>
-      </div>
+      <button className="logout-btn" onClick={() => setShowPopup(true)}>
+        <FaSignOutAlt style={{ marginRight: 8 }} />
+        Logout
+      </button>
+      <PopUpModal 
+        show={showPopup} 
+        onClose={() => setShowPopup(false)}
+      />
     </div>
-    <button className="logout-btn" onClick={onLogout}>
-      <FaSignOutAlt style={{ marginRight: 8 }} />
-      Logout
-    </button>
-  </div>
-);
+  );
+};
 
 export default SettingsSection;
