e8ecca307c97ea65089217a6dcbc1eb2d2576fba
RaphaelRato <raphaelrato2002@gmail.com>
2025-08-19 13:43:22 +0200

    MapChanges

--- frontend/src/pages/MapPage.js
+++ frontend/src/pages/MapPage.js
@@ -1,22 +1,40 @@
-import React, {useEffect, useState} from 'react';
+import React, { useEffect, useState } from 'react';
 import { Container } from 'react-bootstrap';
-import { MapContainer, TileLayer, Marker, LayersControl } from 'react-leaflet';
+import { MapContainer, TileLayer, Marker, LayersControl, useMap } from 'react-leaflet';
 import L from 'leaflet';
 import 'leaflet/dist/leaflet.css';
 import './MapPage.css';
 
-const MapPage = () => {
-  const [currentPosition, setCurrentPosition] = useState(null);  // user current location
-  const defaultPosition  = [-25.8812222, 28.291611111111113]; //starting location - hardcoded for now
-
-  delete L.Icon.Default.prototype._getIconUrl;
-L.Icon.Default.mergeOptions({
-  iconRetinaUrl: require('leaflet/dist/images/marker-icon-2x.png'),
-  iconUrl: require('leaflet/dist/images/marker-icon.png'),
-  shadowUrl: require('leaflet/dist/images/marker-shadow.png'),
+const userIcon = L.icon({
+  iconUrl: require("../assets/user-location.png"),
+  iconSize: [36, 36],
+  iconAnchor: [16, 36],
+  popupAnchor: [0, -36]
 });
 
-useEffect(() => {
+function RecenterMap({ position }) {
+  const map = useMap();
+  useEffect(() => {
+    if (position) {
+      map.setView(position, 16);
+    }
+  }, [position, map]);
+  return null;
+}
+
+const MapPage = () => {
+  const [currentPosition, setCurrentPosition] = useState(null);  // user current location
+  const defaultPosition = [-25.8812222, 28.291611111111113]; // fallback location
+
+  delete L.Icon.Default.prototype._getIconUrl;
+  L.Icon.Default.mergeOptions({
+    iconRetinaUrl: require('leaflet/dist/images/marker-icon-2x.png'),
+    iconUrl: require('leaflet/dist/images/marker-icon.png'),
+    shadowUrl: require('leaflet/dist/images/marker-shadow.png'),
+  });
+
+  // Request user location
+  useEffect(() => {
     if ('geolocation' in navigator) {
       navigator.geolocation.getCurrentPosition(
         (position) => {
@@ -35,17 +53,16 @@ useEffect(() => {
   return (
     <Container className="map-page">
       <MapContainer
-        // center={currentPosition || defaultPosition}
-        center={defaultPosition}  // change this later
+        center={defaultPosition}
         zoom={16}
         scrollWheelZoom={true}
         style={{ height: '100%', width: '100%' }}
-        className='map-container'
+        className="map-container"
       >
         <LayersControl position="topright">
           <LayersControl.BaseLayer name="Normal">
             <TileLayer
-              attribution='&copy; OpenStreetMap contributors' 
+              attribution='&copy; OpenStreetMap contributors'
               url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
             />
           </LayersControl.BaseLayer>
@@ -57,10 +74,14 @@ useEffect(() => {
           </LayersControl.BaseLayer>
         </LayersControl>
 
-        {defaultPosition && (
-          <Marker position={defaultPosition} />
-        )}
+        {/* Default location marker */}
+        <Marker position={defaultPosition} />
 
+        {/* User location marker */}
+        {currentPosition && <Marker position={currentPosition} icon={userIcon} />}
+
+        {/* Recenter when user position updates */}
+        <RecenterMap position={currentPosition} />
       </MapContainer>
     </Container>
   );
