7a6885eac2d05f9ee8cb2226977556b6d4e3c61c
RubenGadd2 <u23633353@tuks.co.za>
2025-08-19 18:59:18 +0200

    detectVideo function for live detection

--- frontend/src/utility/detection.js
+++ frontend/src/utility/detection.js
@@ -48,3 +48,34 @@ export const detectImage = async (imgSource, model, classThreshold, canvasRef) =
 };
 
 
+export const detectVideo = (vidSource, model, classThreshold, canvasRef) => {
+    const [modelWidth, modelHeight] = model.inputShape.slice(1, 3);
+
+
+    // Function detecting every video frame
+
+    const detectFrame = async () => {
+        if(vidSource.videoWidth === 0 && vidSource.srcObject === null){
+            const ctx = canvasRef.getContext("2d");
+            ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height); // Clean canvas
+            return;
+        }
+
+        tf.engine().startScope();
+        const [input, xRatio, yRatio] = preprocess(vidSource, modelWidth, modelHeight);
+
+        await model.net.executeAsync(input).then((res) => {
+            const [boxes, scores, classes] = res.slice(0, 3);
+            const boxes_data = boxes.dataSync();
+            const scores_data = scores.dataSync();
+            const classes_data = classes.dataSync();
+            renderBoxes(canvasRef, classThreshold, boxes_data, scores_data, classes_data, [xRatio, yRatio]);
+            tf.dispose(res);
+        });
+
+        requestAnimationFrame(detectFrame); // get another frame
+        tf.engine().endScope();
+    };
+
+    detectFrame(); // initialize to detect every frame
+};
