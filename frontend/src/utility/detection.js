c016d9fc0793f3f3573e812ea376aa6108a87133
RubenGadd2 <u23633353@tuks.co.za>
2025-09-15 17:34:14 +0200

    Redid detectVideo Function
    Adjusting to YOLOv11 Output format

--- frontend/src/utility/detection.js
+++ frontend/src/utility/detection.js
@@ -86,3 +86,84 @@ export const detectImage = async (imgSource, model, classThreshold, canvasRef) =
 };
 
 
+export const detectVideo = (vidSource, model, classThreshold, canvasRef) => {
+    const [modelWidth, modelHeight] = model.inputs[0].shape.slice(1, 3);
+    console.log("Model input size:", modelWidth, modelHeight);
+
+    const detectFrame = async () => {
+        console.log("detectFrame called...");
+
+        if( vidSource.videoWidth === 0 || vidSource.videoHeight === 0) {
+            requestAnimationFrame(detectFrame)
+            return;
+        }
+
+        tf.engine().startScope();
+
+        const [input, xRatio, yRatio] = preprocess(vidSource, modelWidth, modelHeight);
+        console.log("Input tensor shape:", input.shape);
+
+        try {
+            const res = await model.execute(input); // In yolov11. Expected tensor output is [1, N, C]. N is based on 640 x 640 = 8400. C is dependent on the classes count + 5 if im not mistaken
+            console.log("Raw output tensor:", res);
+
+            const outputData = res.dataSync();
+            console.log("Output data length:", outputData.length);
+
+            const numClasses = res.shape[2] - 5;
+            console.log("Number of detections:", numClasses);
+
+            const boxes = [];
+            const scores = [];
+            const classes = [];
+
+            for (let i = 0; i < res.shape[1]; i++) {
+                const offset = i * (5 + numClasses);
+
+                const x = outputData[offset];
+                const y = outputData[offset + 1];
+                const w = outputData[offset + 2];
+                const h = outputData[offset + 3];
+                const objectness = outputData[offset + 4];
+                
+                let bestClass = -1;
+                let bestScore = 0;
+
+                for (let c = 0; c < numClasses; c++) {
+                    const classProb = outputData[offset + 5 + c];
+                    const score = objectness * classProb;
+                    if ( score > bestScore) {
+                        bestScore = score;
+                        bestClass = c;
+                    }
+                }
+                
+                if( bestScore > classThreshold) {
+                    const x1 = x - w / 2;
+                    const y1 = x - h / 2;
+                    const x2 = x + w / 2;
+                    const y2 = x + h / 2;
+
+                    boxes.push([x1, y1, x2, y2]);
+                    scores.push(bestScore);
+                    classes.push(bestClass);
+                }
+
+            }
+
+            console.log("First 5 boxes:", boxes.slice(0, 5));
+            console.log("First 5 scores:", scores.slice(0, 5));
+            console.log("First 5 classes:", classes.slice(0, 5));
+
+            renderBoxes(canvasRef, classThreshold, boxes.flat(), scores, classes, [xRatio, yRatio]);
+            tf.dispose(res);
+        } catch (err) {
+            console.error("Error during model execution:", err);
+        }
+
+        requestAnimationFrame(detectFrame);
+        tf.engine().endScope();
+    };
+
+    detectFrame();
+};
