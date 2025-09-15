3688e7a0de05433221e72cac9829d9e064e5fde0
RubenGadd2 <u23633353@tuks.co.za>
2025-09-15 11:06:05 +0200

    Some cleanup and new model exported as tfjs

--- AI/python/pytorch_to_tensorflow.py
+++ AI/python/pytorch_to_tensorflow.py
@@ -2,7 +2,7 @@ from ultralytics import YOLO
 import tensorflowjs as tfjs
 
 PT_MODEL = YOLO("my_model.pt")
-NEW_MODEL_NAME =  "model_v0.2"
+NEW_MODEL_NAME =  "model_v3"
 
 # Export PyTorch YOLO model directly to TensorFlow SavedModel
 tf_model_path = PT_MODEL.export(format="tfjs" )
