f244a2cf7ee2a4069334dcd5a7b904a2da2908fb
Tom Schulz <91066892+tomschulz02@users.noreply.github.com>
2025-09-22 11:22:22 +0200

    Added repository functions for user preferences

---
+++ api/src/ProfileServer/profileRepository.js
@@ -0,0 +1,83 @@
+import db from '../db/index';
+
+async function updateThemePreference(user, preference) {
+	let query = '';
+	if (await userPreferencesExist(user)) {
+		query = 'UPDATE user_preferences SET theme=$2 WHERE id=$1;';
+	} else {
+		query = 'INSERT INTO user_preferences (id, theme) VALUES ($1, $2);';
+	}
+
+	try {
+		const result = await db.query(query, [user, preference]);
+
+		if (result.rowCount > 0) {
+			return 'PREFERENCES_SET';
+		} else {
+			return 'DB_ERROR';
+		}
+	} catch (error) {
+		console.error(error);
+		return 'DB_ERROR';
+	}
+}
+
+async function updateNotificationPreference(user, preference) {
+	let query = '';
+	if (await userPreferencesExist(user)) {
+		query = 'UPDATE user_preferences SET enable_notifications=$2 WHERE id=$1;';
+	} else {
+		query = 'INSERT INTO user_preferences (id, enable_notifications) VALUES ($1, $2);';
+	}
+
+	try {
+		const result = await db.query(query, [user, preference]);
+
+		if (result.rowCount > 0) {
+			return 'PREFERENCES_SET';
+		} else {
+			return 'DB_ERROR';
+		}
+	} catch (error) {
+		console.error(error);
+		return 'DB_ERROR';
+	}
+}
+
+async function updateLocationPreference(user, preference) {
+	let query = '';
+	if (await userPreferencesExist(user)) {
+		query = 'UPDATE user_preferences SET enable_location=$2 WHERE id=$1;';
+	} else {
+		query = 'INSERT INTO user_preferences (id, enable_location) VALUES ($1, $2);';
+	}
+
+	try {
+		const result = await db.query(query, [user, preference]);
+
+		if (result.rowCount > 0) {
+			return 'PREFERENCES_SET';
+		} else {
+			return 'DB_ERROR';
+		}
+	} catch (error) {
+		console.error(error);
+		return 'DB_ERROR';
+	}
+}
+
+async function userPreferencesExist(user) {
+	const result = await db.query('SELECT * FROM user_preferences WHERE id=$1;', [user]);
+
+	if (result.rowCount > 0) {
+		return true;
+	} else {
+		return false;
+	}
+}
+
+export const profileRepo = {
+	updateThemePreference,
+	updateNotificationPreference,
+	updateLocationPreference,
+};
